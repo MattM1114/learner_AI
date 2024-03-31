250291b4288e099c3b56e420bbd5fbbf69636dd1
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2024-03-31 05:35:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify, request
-from chatbot import chat  # Import your chatbot function from the existing Python code
+from chatbot import chat  # Ensure this imports your chat function correctly
+from flask_cors import CORS  # Import CORS if you're serving the frontend from a different origin
+from waitress import serve
+
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all domains on all routes
+serve(app, host='0.0.0.0', port=8080)
 
 @app.route('/query', methods=['POST'])
 def handle_query():
@@ -12,3 +17,4 @@ def handle_query():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
